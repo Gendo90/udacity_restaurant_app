21dcb027ad2a4aafb8a2a4d8e25c34d887f42045
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-10-10 12:39:17 -0700

    feat: Add alt text (and title) to images for this project

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -161,6 +161,9 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  let imgText = DBHelper.imageAltForRestaurant(restaurant);
+  image.setAttribute("alt", imgText);
+  image.setAttribute("title", imgText);
   li.append(image);
 
   const name = document.createElement('h1');
@@ -197,7 +200,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-} 
+}
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -208,4 +211,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
